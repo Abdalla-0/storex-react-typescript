917dcbbfe5d6b5c93d562b088475211df37992d4
Abdalla Atef <136491297+Abdalla-0@users.noreply.github.com>
2024-09-22 07:13:54 +0300

    up

--- src/components/common/Modal/Popup.tsx
+++ src/components/common/Modal/Popup.tsx
@@ -2,23 +2,21 @@ import { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 
 export const MeasurementsPopup = () => {
-  const [show, setShow] = useState(true);
-
-  const handleClose = () => setShow(false);
-  //   const handleShow = () => setShow(true);
+  const [modalToggler, setModalToggler] = useState(false);
+  const handleToggle = () => setModalToggler(false);
 
   return (
     <>
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={modalToggler} onHide={handleToggle}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={handleToggle}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={handleToggle}>
             Save Changes
           </Button>
         </Modal.Footer>
--- src/components/ui/Products/ProductsView/ProductsView.tsx
+++ src/components/ui/Products/ProductsView/ProductsView.tsx
@@ -1,8 +1,8 @@
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Modal, Row } from "react-bootstrap";
 import { useAppDispatch, useAppSelector } from "../../../../store/hook";
 import { actionGetProducts } from "../../../../store/Products/productsSlice";
 import styles from "./style.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { TProduct } from "../../../../types";
 const { product } = styles;
 
@@ -12,14 +12,27 @@ const ProductsView = ({ getItem }: { getItem: (item: TProduct) => void }) => {
   const dispatch = useAppDispatch();
   const { data } = useAppSelector((state) => state.products);
 
+  const [modalToggler, setModalToggler] = useState(false);
+  const handleToggle = () => setModalToggler(false);
+
+  const [productTilte, setProductTilte] = useState()
+
+  const titleHandler = (item: TProduct) => {
+    setProductTilte(item)
+    console.log(productTilte);
+    
+  }
+
   useEffect(() => {
     dispatch(actionGetProducts());
   }, [dispatch]);
 
   const addToCartHandler = (item: TProduct) => {
-    getItem(item);
     if (item.measurements.length > 1) {
-      console.log("Yes");
+      setModalToggler(true);
+      titleHandler(item)
+    } else {
+      getItem(item);
     }
   };
 
@@ -28,12 +41,51 @@ const ProductsView = ({ getItem }: { getItem: (item: TProduct) => void }) => {
       <Row className="row-gap-3">
         {data &&
           data.map((el) => (
-            <Col xs={6} xl={4} key={el.product_id}>
-              <div className={product} onClick={() => addToCartHandler(el)}>
-                {el.product_name}
-              </div>
-            </Col>
+            <>
+              <Col xs={6} xl={4} key={el.product_id}>
+                <div className={product} onClick={() => addToCartHandler(el)}>
+                  {el.product_name}
+                </div>
+              </Col>
+            </>
           ))}
+
+        <Modal size="lg" show={modalToggler} onHide={handleToggle}>
+          <Modal.Header closeButton>
+          {data && data.map((el) => (<Modal.Title key={el.product_id}>{el.product_name}</Modal.Title>)}
+          </Modal.Header>
+          <Modal.Body>
+            <Row
+              className="fw-bold py-3"
+              style={{ borderBlockEnd: "1px solid #d5d5d5" }}
+            >
+              <Col>الخصائص</Col>
+              <Col xs={2}>السعر</Col>
+              <Col xs={2}>الكمية</Col>
+              <Col xs={3}></Col>
+            </Row>
+            {data.map((el) =>
+              el.measurements.map((measurement) => (
+                <Row
+                  key={measurement.measurement_id}
+                  className="py-2"
+                  style={{ borderBlockEnd: "1px solid #d5d5d5" }}
+                >
+                  <Col>{measurement.nickname}</Col>
+                  <Col xs={2}>
+                    {measurement.price ? measurement.price : "غير مسعر"}
+                  </Col>
+                  <Col xs={2}>311</Col>
+                  <Col xs={3}>
+                    <Button className="w-100" variant="primary">
+                      اختيار
+                    </Button>
+                  </Col>
+                </Row>
+              ))
+            )}
+          </Modal.Body>
+        </Modal>
       </Row>
     </div>
   );
